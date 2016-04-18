3d8617b7380d26a3071a3da472fb1bc83a6b9b41
BestOrNothing <1562490789@qq.com>
2016-04-18 20:58:07 +0800

    sql文件

---
+++ code/server/RunningCat.sql
@@ -0,0 +1,29 @@
+create database if not exists RunningCat;
+use RunningCat;
+drop table if exists Running;
+drop table if exists User;
+create table User(
+    age         integer,
+    height      integer,
+    weight      float, 
+    sex         char(1),
+    nickname    varchar(20),
+    location    varchar(20),
+    catExp      integer,
+    catFood     integer,
+    id          char(50) primary key,
+    createDate  date,
+    password    varchar(50),
+    allDist     float
+);
+drop table if exists Day;
+create table Day(
+    date        date primary key   
+);
+create table Running(
+    id          char(50), 
+    date        date,     
+    dist        float,
+    foreign key(date) references Day(date),
+    foreign key(id) references User(id)
+);
